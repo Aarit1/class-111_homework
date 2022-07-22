16ad8c66ca3b9462f2f13f83f73cfd4d9a46d419
Aarit1 <85659004+Aarit1@users.noreply.github.com>
2022-07-21 22:39:46 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+Prediction = "";
+
+Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+
+console.log('ml5 version:', ml5.version)
+
+classifer = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/J9r5majSk/model.json');
